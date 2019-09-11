39859e94525dc7ff26798938f8deffb541f3c264
Erik Langenborg <erik.langenborg@rseg.com>
2019-09-10 23:19:18 -0400

    Added notes and new function

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+target/wheels/string_sum-0.1.0-cp37-cp37m-macosx_10_7_x86_64.whl:
+	maturin build
+
+install: target/wheels/string_sum-0.1.0-cp37-cp37m-macosx_10_7_x86_64.whl
+	-yes | pip uninstall string-sum
+	pip install ./target/wheels/string_sum-0.1.0-cp37-cp37m-macosx_10_7_x86_64.whl
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 
-#![allow(unused_variables)]
-fn main() {
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-
+use rayon::prelude::*;
+use std::fs;
+use std::path::PathBuf;
 
 #[pyfunction]
 /// Formats the sum of two numbers as string
@@ -11,11 +11,94 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+/// Junk
+fn torres(a: f64, b: f64) -> PyResult<String> {
+    Ok((a * b).to_string())
+}
+
+
+
+/// Represents a file that can be searched
+#[pyclass(module = "word_count")]
+struct WordCounter {
+    path: PathBuf,
+}
+
+#[pymethods]
+impl WordCounter {
+    #[new]
+    fn new(obj: &PyRawObject, path: String) {
+        obj.init(WordCounter {
+            path: PathBuf::from(path),
+        });
+    }
+
+    /// Searches for the word, parallelized by rayon
+    fn search(&self, py: Python<'_>, search: String) -> PyResult<usize> {
+        let contents = fs::read_to_string(&self.path)?;
+
+        let count = py.allow_threads(move || {
+            contents
+                .par_lines()
+                .map(|line| count_line(line, &search))
+                .sum()
+        });
+        Ok(count)
+    }
+
+    /// Searches for a word in a classic sequential fashion
+    fn search_sequential(&self, needle: String) -> PyResult<usize> {
+        let contents = fs::read_to_string(&self.path)?;
+
+        let result = contents.lines().map(|line| count_line(line, &needle)).sum();
+
+        Ok(result)
+    }
+}
+
+fn matches(word: &str, needle: &str) -> bool {
+    let mut needle = needle.chars();
+    for ch in word.chars().skip_while(|ch| !ch.is_alphabetic()) {
+        match needle.next() {
+            None => {
+                return !ch.is_alphabetic();
+            }
+            Some(expect) => {
+                if ch.to_lowercase().next() != Some(expect) {
+                    return false;
+                }
+            }
+        }
+    }
+    return needle.next().is_none();
+}
+
+/// Count the occurrences of needle in line, case insensitive
+#[pyfunction]
+fn count_line(line: &str, needle: &str) -> usize {
+    let mut total = 0;
+    for word in line.split(' ') {
+        if matches(word, needle) {
+            total += 1;
+        }
+    }
+    total
+}
+
+
+
+
+
+
 /// This module is a python module implemented in Rust.
 #[pymodule]
 fn string_sum(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
+    m.add_wrapped(wrap_pyfunction!(torres))?;
+
+    m.add_wrapped(wrap_pyfunction!(count_line))?;
+    m.add_class::<WordCounter>()?;
 
     Ok(())
-}
 }
