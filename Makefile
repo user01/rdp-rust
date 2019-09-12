c66dcab9891281cb6161cdaf2b4403dab980ba56
Erik Langenborg <erik.langenborg@rseg.com>
2019-09-11 23:04:56 -0400

    Added testing notes

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 
+build:
+	maturin build
+
+test:
+	cargo test --no-default-features
+
 target/wheels/string_sum-0.1.0-cp37-cp37m-macosx_10_7_x86_64.whl:
 	maturin build
 
 install: target/wheels/string_sum-0.1.0-cp37-cp37m-macosx_10_7_x86_64.whl
 	-yes | pip uninstall string-sum
 	pip install ./target/wheels/string_sum-0.1.0-cp37-cp37m-macosx_10_7_x86_64.whl
-
--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,41 @@ use rayon::prelude::*;
 use std::fs;
 use std::path::PathBuf;
 
-use ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use ndarray::{ArrayD, ArrayViewD};
+// use ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
 use numpy::{IntoPyArray, PyArrayDyn};
 use pyo3::prelude::{pymodule, Py, PyModule, PyResult, Python};
+
+
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// This is a really bad adding function, its purpose is to fail in this
+// example.
+#[allow(dead_code)]
+fn bad_add(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_bad_add() {
+        // This assert would fire and test will fail.
+        // Please note, that private functions can be tested too!
+        assert_eq!(bad_add(1, 2), 3);
+    }
+}
 
 fn axpy(a: f64, x: ArrayViewD<f64>, y: ArrayViewD<f64>) -> ArrayD<f64> {
     a * &x + &y
