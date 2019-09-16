34a82d40e01cd36ed82ce0507cb23d3367a0412f
Erik Langenborg <erik.langenborg@rseg.com>
2019-09-15 23:23:15 -0400

    Formatting

--- test_options.py
+++ test_options.py
@@ -5,11 +5,12 @@ from rdp import rdp
 
 
 def test_two():
-    arr = np.array([[1., 1.], [5.0, 5.0]])
+    arr = np.array([[1.0, 1.0], [5.0, 5.0]])
     res_rs = rdp_rust.reduce_points(arr, 0.1)
     res_py = rdp(arr, 0.1)
     assert res_rs.shape == res_py.shape
     np.testing.assert_almost_equal(res_rs, res_py)
+
 
 def test_basic():
     arr = np.array([5.0, 0, 4, 0, 3, 0, 3, 1, 3, 2]).reshape(5, 2)
@@ -19,70 +20,169 @@ def test_basic():
     np.testing.assert_almost_equal(res_rs, res_py)
 
 
-
 small_arr = np.array([5.0, 0, 4, 0, 3, 0, 3, 1, 3, 2]).reshape(5, 2)
 small_expected = np.array([5.0, 0, 3, 0, 3, 2.0]).reshape(3, 2)
 small_epsilon = 0.1
 
-@pytest.mark.benchmark(
-    group="small-points",
-    warmup=False,
-)
+
+def test_small_line():
+    for epsilon in np.arange(0.1, 30.0, 0.1):
+        result_py = rdp(small_arr, epsilon)
+        result_rust = rdp_rust.reduce_points(small_arr, epsilon)
+        assert result_py.shape == result_rust.shape, f"epsilon: {epsilon}"
+        np.testing.assert_almost_equal(result_py, result_rust)
+
+
+@pytest.mark.benchmark(group="small-points", warmup=True)
 def test_rust_version(benchmark):
     actual = benchmark(rdp_rust.reduce_points, small_arr, small_epsilon)
     assert actual.shape == small_expected.shape
     np.testing.assert_almost_equal(actual, small_expected)
 
-@pytest.mark.benchmark(
-    group="small-points",
-    warmup=False,
-)
+
+@pytest.mark.benchmark(group="small-points", warmup=True)
 def test_py_version(benchmark):
     actual = benchmark(rdp, small_arr, small_epsilon)
     assert actual.shape == small_expected.shape
     np.testing.assert_almost_equal(actual, small_expected)
 
-medium_arr = np.array([44, 95, 26, 91, 22, 90, 21, 90,
-    19, 89, 17, 89, 15, 87, 15, 86, 16, 85,
-    20, 83, 26, 81, 28, 80, 30, 79, 32, 74,
-    32, 72, 33, 71, 34, 70, 38, 68, 43, 66,
-    49, 64, 52, 63, 52, 62, 53, 59, 54, 57,
-    56, 56, 57, 56, 58, 56, 59, 56, 60, 56,
-    61, 55, 61, 55, 63, 55, 64, 55, 65, 54,
-    67, 54, 68, 54, 76, 53, 82, 52, 84, 52,
-    87, 51, 91, 51, 93, 51, 95, 51, 98, 50,
-    105, 50, 113, 49, 120, 48, 127, 48, 131, 47,
-    134, 47, 137, 47, 139, 47, 140, 47, 142, 47,
-    145, 46, 148, 46, 152, 46, 154, 46, 155, 46,
-    159, 46, 160, 46, 165, 46, 168, 46, 169, 45,
-    171, 45, 173, 45, 176, 45, 182, 45, 190, 44,
-    204, 43, 204, 43, 207, 43, 215, 40, 215, 38,
-    215, 37, 200, 37, 195, 41.0]).reshape(-1, 2)
-medium_expected = np.array([[44.0, 95.0], [22.0, 90.0], [21.0, 90.0], [19.0, 89.0], [17.0, 89.0], [15.0, 87.0], [15.0, 86.0], [16.0, 85.0], [20.0, 83.0], [26.0, 81.0], [30.0, 79.0], [32.0, 74.0], [32.0, 72.0], [34.0, 70.0], [38.0, 68.0], [43.0, 66.0], [52.0, 63.0], [52.0, 62.0], [53.0, 59.0], [54.0, 57.0], [56.0, 56.0], [60.0, 56.0], [61.0, 55.0], [64.0, 55.0], [65.0, 54.0], [68.0, 54.0], [76.0, 53.0], [82.0, 52.0], [84.0, 52.0], [87.0, 51.0], [95.0, 51.0], [98.0, 50.0], [105.0, 50.0], [120.0, 48.0], [127.0, 48.0], [131.0, 47.0], [142.0, 47.0], [145.0, 46.0], [168.0, 46.0], [169.0, 45.0], [182.0, 45.0], [190.0, 44.0], [204.0, 43.0], [207.0, 43.0], [215.0, 40.0], [215.0, 37.0], [200.0, 37.0], [195.0, 41.0]])
-medium_epsilon = 0.1
 
 def test_medium_line():
-    for epsilon in np.arange(0.1, 30.0, 0.1):
+    for epsilon in np.linspace(0.9, 0.005, 50):
         result_py = rdp(medium_arr, epsilon)
         result_rust = rdp_rust.reduce_points(medium_arr, epsilon)
-
-        # np.testing.assert_almost_equal(result_py, result_rust)
         assert result_py.shape == result_rust.shape, f"epsilon: {epsilon}"
+        np.testing.assert_almost_equal(result_py, result_rust)
 
-@pytest.mark.benchmark(
-    group="medium-points",
-    warmup=False,
-)
+
+@pytest.mark.benchmark(group="medium-points", warmup=True)
 def test_rust_version(benchmark):
     actual = benchmark(rdp_rust.reduce_points, medium_arr, medium_epsilon)
     assert actual.shape == medium_expected.shape
     np.testing.assert_almost_equal(actual, medium_expected)
 
-@pytest.mark.benchmark(
-    group="medium-points",
-    warmup=False,
-)
+
+@pytest.mark.benchmark(group="medium-points", warmup=True)
 def test_py_version(benchmark):
     actual = benchmark(rdp, medium_arr, medium_epsilon)
     assert actual.shape == medium_expected.shape
     np.testing.assert_almost_equal(actual, medium_expected)
+
+
+medium_arr = np.array(
+    [
+        18.71120367913943,
+        35.67559590537299,
+        19.87575446038943,
+        37.21327118250812,
+        20.33718024163943,
+        37.19577035281653,
+        21.23805914788943,
+        37.0556176917564,
+        21.41384039788943,
+        36.6512354470846,
+        21.67751227288943,
+        36.580689928979275,
+        22.00710211663943,
+        36.633605119022356,
+        22.05104742913943,
+        36.26243735259619,
+        22.31471930413943,
+        36.52773844590173,
+        22.64430914788943,
+        36.085067452949716,
+        23.03981696038943,
+        36.28015225239681,
+        23.23757086663943,
+        35.8716942147415,
+        23.76491461663943,
+        36.067308390223566,
+        23.80885992913943,
+        35.67559590537299,
+        24.27028571038943,
+        35.76479144718892,
+        24.53395758538943,
+        35.35366637248113,
+        25.23708258538943,
+        35.960669831102834,
+        23.89675055413943,
+        37.31819090754369,
+        22.13893805413943,
+        37.87528917447911,
+        20.97438727288943,
+        38.41099084386128,
+        18.90895758538943,
+        37.87528917447911,
+        17.94216071038943,
+        37.03808036667257,
+        17.21706305413943,
+        35.44322069725778,
+        17.61257086663943,
+        33.88911578982415,
+        19.37038336663943,
+        34.18045430429265,
+        20.09548102288943,
+        35.47901456666836,
+        21.04030524163943,
+        36.13832057287443,
+        21.61159430413943,
+        35.746960327835474,
+        21.61159430413943,
+        35.31781680026504,
+        22.13893805413943,
+        35.639889738821545,
+        22.13893805413943,
+        35.21017271507273,
+        22.75417242913943,
+        35.44322069725778,
+        22.75417242913943,
+        35.01245420877086,
+        23.39137946038943,
+        35.28195132861475,
+        23.43532477288943,
+        34.814256575733275,
+        24.00661383538943,
+        35.10238572512127,
+        24.07253180413943,
+        34.705947263853155,
+    ]
+).reshape(-1, 2)
+medium_expected = np.array(
+    [
+        [18.71120368, 35.67559591],
+        [19.87575446, 37.21327118],
+        [21.23805915, 37.05561769],
+        [21.4138404, 36.65123545],
+        [22.00710212, 36.63360512],
+        [22.05104743, 36.26243735],
+        [22.3147193, 36.52773845],
+        [22.64430915, 36.08506745],
+        [23.03981696, 36.28015225],
+        [23.23757087, 35.87169421],
+        [23.76491462, 36.06730839],
+        [23.80885993, 35.67559591],
+        [24.27028571, 35.76479145],
+        [24.53395759, 35.35366637],
+        [25.23708259, 35.96066983],
+        [23.89675055, 37.31819091],
+        [20.97438727, 38.41099084],
+        [18.90895759, 37.87528917],
+        [17.94216071, 37.03808037],
+        [17.21706305, 35.4432207],
+        [17.61257087, 33.88911579],
+        [19.37038337, 34.1804543],
+        [20.09548102, 35.47901457],
+        [21.04030524, 36.13832057],
+        [21.6115943, 35.74696033],
+        [21.6115943, 35.3178168],
+        [22.13893805, 35.63988974],
+        [22.13893805, 35.21017272],
+        [22.75417243, 35.4432207],
+        [22.75417243, 35.01245421],
+        [23.39137946, 35.28195133],
+        [23.43532477, 34.81425658],
+        [24.00661384, 35.10238573],
+        [24.0725318, 34.70594726],
+    ]
+)
+medium_epsilon = 0.1
