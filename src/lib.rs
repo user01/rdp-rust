8eb6f5098c57ac9bdced3575dbafc43cd889f8bf
Erik Langenborg <erik.langenborg@gmail.com>
2019-09-24 22:46:24 -0400

    Basic help for the function

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,17 @@ use pyo3::{IntoPy, PyObject, ToPyObject};
 
 mod rdp;
 
-
+/// reduce_points(points, epsilon, /)
+/// --
+///
+/// Perform RDP point reduction on the float64 numpy array of points
+/// points must be a numpy array of n by d, where n is the number
+/// of points and d is the dimension.
+/// Epsilon (float) is the threshold where points are removed as
+/// irrelevant, as defined by euclidean distance in the d
+/// dimensional space
+/// Makes use of the Ramer-Douglas-Peucker algorithm
+/// https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
 #[pyfunction]
 fn reduce_points(
     py: Python,
@@ -29,8 +39,6 @@ fn reduce_points(
 }
 
 
-
-/// This module is a python module implemented in Rust.
 #[pymodule]
 fn rdp_rust(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(reduce_points))?;
